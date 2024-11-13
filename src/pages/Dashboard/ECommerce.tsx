261e685a3a82feb55ec381c043465445e83cc1fc
NicolasV7 <159790870+NicolasV7@users.noreply.github.com>
2024-11-12 20:25:58 -0500

    Update ECommerce.tsx

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -61,7 +61,7 @@ const ECommerce: React.FC = () => {
   return (
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardDataStats title="Subscripciones Totales uwu" total={totalSubscribers.toString()} rate={subscribersPercentage} levelDown={parseFloat(usersPercentage) < 0}
+        <CardDataStats title="Subscripciones Totales" total={totalSubscribers.toString()} rate={subscribersPercentage} levelDown={parseFloat(usersPercentage) < 0}
         levelUp={parseFloat(usersPercentage) >= 0}  >
           <svg
             className="fill-primary dark:fill-white"
